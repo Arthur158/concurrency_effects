be8f6c18a58e0fa77bf9963ae338ee58b4b6c2fe
Arthur158 <96660803+Arthur158@users.noreply.github.com>
2024-05-27 12:55:20 +0200

    polishing

--- src/Choose.hs
+++ src/Choose.hs
@@ -25,6 +25,7 @@ import Control.Applicative
 import Control.Monad
 
 -- Effect for nondeterminism. Choose allows for a branching into a "True" branch and/or a "False" branch.
+-- Pick is the same as choose but for a range of numbers
 -- Zero is denoting an empty result.
 data Choose k 
   = Choose (Bool -> k)
@@ -32,15 +33,16 @@ data Choose k
   | Zero
   deriving Functor
 
--- Smart constructor for choose
+-- Smart constructor for Choose
 choose :: Choose <: f => Free f Bool
 choose = Op (inj' (Choose Pure))
 
+-- Smart constructor for Pick
 pick :: Choose <: f => Int -> Free f Int
 pick a = Op (inj' (Pick a Pure))
 
 
--- Smart constructor for zero
+-- Smart constructor for Zero
 zero :: Choose <: f => Free f a
 zero = Op (inj' Zero)
 
@@ -53,7 +55,7 @@ hChooseTrue = Handler
     Zero -> pure Nothing} -- here somehow feed f True or/and false
 
 
--- Handler that makes a list accumulating the results of the "True" and "False" branches. Zeroes are discarded.
+-- Nondeterministic handler for Choose. Accumulates the results of all the branches in a list
 hChoose' :: Functor f' => Handler Choose a f' [a]
 hChoose' = Handler
   { ret = \x -> pure [x]
@@ -62,11 +64,17 @@ hChoose' = Handler
                Pick n f -> foldM (\acc i -> fmap (++ acc) (f i)) [] [0..n-1]
                Zero -> pure []}
 
--- Function for choosing nondeterministically between two continuation
+-- Syntactic sugar for choosing between two continuations using Choose
 (~+~) :: Choose <: f => Free f a -> Free f a -> Free f a
 m1 ~+~ m2 = do
   b <- choose
   if b then m1 else m2
+
+-- Syntactic sugar for choosing from a list of continuations using Choose
+sum :: Choose <: f => [Free f a] -> Free f a
+sum xs= do
+  m <- pick (length xs)
+  xs !! m
 
 -- Using the Choose effect to make an instance of Alternative for Free f
 instance (Functor f, Choose <: f) => Alternative (Free f) where
